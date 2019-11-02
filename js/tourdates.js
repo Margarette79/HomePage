d6c5bfc30ab304d9026a1d93fa929304245601e3
margarette79 <margarette.osorio@gmail.com>
2019-11-02 02:20:14 -0700

    All Pages

---
+++ js/tourdates.js
@@ -0,0 +1,77 @@
+$( document ).ready(function() {
+    //alert("Hello friend!!!")
+
+    console.log($('#tickets').css('color'));
+    $('#tickets').click(function() {
+      if ($('#tickets').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets').css('color', 'red');
+      } else {
+        $('#tickets').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+    console.log($('#tickets1').css('color'));
+    $('#tickets1').click(function() {
+      if ($('#tickets1').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets1').css('color', 'red');
+      } else {
+        $('#tickets1').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+    console.log($('#tickets2').css('color'));
+    $('#tickets2').click(function() {
+      if ($('#tickets2').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets2').css('color', 'red');
+      } else {
+        $('#tickets2').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+    console.log($('#tickets3').css('color'));
+    $('#tickets3').click(function() {
+      if ($('#tickets3').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets3').css('color', 'red');
+      } else {
+        $('#tickets3').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+    console.log($('#tickets4').css('color'));
+    $('#tickets4').click(function() {
+      if ($('#tickets4').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets4').css('color', 'red');
+      } else {
+        $('#tickets4').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+    console.log($('#tickets5').css('color'));
+    $('#tickets5').click(function() {
+      if ($('#tickets5').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets5').css('color', 'red');
+      } else {
+        $('#tickets5').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+    console.log($('#tickets6').css('color'));
+    $('#tickets6').click(function() {
+      if ($('#tickets6').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets6').css('color', 'red');
+      } else {
+        $('#tickets6').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+    console.log($('#tickets7').css('color'));
+    $('#tickets7').click(function() {
+      if ($('#tickets7').css('color') == "rgb(254, 254, 254)") {
+        $('#tickets7').css('color', 'red');
+      } else {
+        $('#tickets7').css('color', "rgb(254, 254, 254)");
+      }
+    })
+
+
+ });
