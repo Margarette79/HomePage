2e460a85993da1e528f2b2825c0a14b4fddcdfa8
margarette79 <margarette.osorio@gmail.com>
2019-12-07 08:30:40 -0800

    Fix Page

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,15 @@
+
+function myFunction() {
+  var txt;
+  if (confirm("BUY YOUR TICKET!")) {
+    txt = "Bellow choose the date and place";
+  } else {
+    txt = "";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -7,3 +19,17 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+$( document ).ready(function (){
+    var $btns = $('.btn').click(function(){
+      if(this.id == 'all') {
+        $('#basket > div').show();
+      } else {
+        var $el = $('.' + this.id).show();
+        $('#basket > div').not($el).hide();
+      }
+        $btns.removeClass('active');
+        $(this).addClass('active');
+    })
+})
